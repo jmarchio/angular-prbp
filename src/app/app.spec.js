abb4876e67e49e2f024612f25dbc7aaa1cbf88c5
Juan Marchioli <juan@mobomo.com>
2016-10-24 00:33:50 -0300

    kickoff

---
+++ src/app/app.spec.js
@@ -0,0 +1,22 @@
+describe( 'AppCtrl', function() {
+  describe( 'isCurrentUrl', function() {
+    var AppCtrl, $location, $scope;
+
+    beforeEach( module( 'ngBoilerplate' ) );
+
+    beforeEach( inject( function( $controller, _$location_, $rootScope ) {
+      $location = _$location_;
+      $scope = $rootScope.$new();
+      AppCtrl = $controller( 'AppCtrl', { $location: $location, $scope: $scope });
+    }));
+
+    it( 'should pass a dummy test', inject( function() {
+      console.log(AppCtrl);
+      expect( AppCtrl ).toBeTruthy();
+    }));
+
+    /*it( 'should pass a dummy test', inject( function() {
+      expect( true ).toBeTruthy();
+    }));*/
+  });
+});
